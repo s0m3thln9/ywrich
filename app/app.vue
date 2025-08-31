5beeb8bb521e6b61ecf1087d16b89c1a52a002c1
s0m3thlin9 <dunlock67@gmail.com>
2025-08-31 13:53:50 +0300

    add advertiser and some pictures

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="relative">
-    <div class="absolute w-full min-h-screen h-full -z-10 bg-[#090909] overflow-hidden top-0 left-0">
+    <div
+	    class="absolute w-full min-h-screen h-full -z-10 bg-[#090909] overflow-hidden top-0 left-0"
+    >
       <NuxtImg
-        src="/lines-1.svg"
-        class="absolute top-0 left-0 w-full h-full object-cover z-0"
+	      src="/lines-1.svg"
+	      class="absolute top-0 left-0 w-full object-cover z-0"
       />
+	    <NuxtImg
+		    src="/lines-2.svg"
+		    class="absolute top-0 translate-y-full left-0 w-full object-cover z-0"
+	    />
       <NuxtImg
-        src="/cloud-1.png"
-        class="absolute top-0 left-0 w-full z-20"
+	      src="/cloud-1.png"
+	      class="absolute top-0 left-0 w-full z-20"
       />
 	    <div class="absolute top-10 -right-[60px]">
 		    <div class="relative w-[1140px] aspect-[1140/949]">
@@ -17,7 +23,7 @@
 			    />
 		      <NuxtImg
 			      src="/cloud-2.png"
-			      class="absolute -bottom-1/3 right-0 transform  w-[1140px] z-20"
+			      class="absolute -bottom-1/3 right-0 w-[1140px] z-20"
 		      />
 		    </div>
 	    </div>
@@ -25,6 +31,7 @@
     <AppHeader />
     <AppHero />
 	  <AppAbout />
+	  <AppAdvertiser />
   </div>
 </template>
 <script setup lang="ts">
